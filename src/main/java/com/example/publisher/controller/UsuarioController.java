1b1a82a9339b4a537f11fd15cba1c4f4d31d70d1
tamagochita <mevere66@gmail.com>
2022-04-12 12:02:02 -0500

    endpoints funcionales

--- src/main/java/com/example/publisher/controller/UsuarioController.java
+++ src/main/java/com/example/publisher/controller/UsuarioController.java
@@ -1,12 +1,41 @@
 package com.example.publisher.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.publisher.model.DTO.UsuarioDTO;
+import com.example.publisher.model.UsuarioModel;
+import com.example.publisher.service.UsuarioService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 
+    @Autowired
+    ModelMapper mapper;
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping("/listarUsuarios")
+    public Flux<UsuarioDTO> findAll() {
+        return this.usuarioService.listarTodos().flatMap(c ->
+                Mono.just(mapper.map(c, UsuarioDTO.class)));
+
+    }
+
+
+    @PostMapping("/crearUsuario")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<UsuarioModel> saveClient(@RequestBody UsuarioDTO usuarioDTO) {
+        var usuario = mapper.map(usuarioDTO, UsuarioModel.class);
+
+        return this.usuarioService.saveUsuario(usuario);
+
+    }
 
 }
--- src/main/java/com/example/publisher/service/UsuarioService.java
+++ src/main/java/com/example/publisher/service/UsuarioService.java
@@ -1,8 +1,27 @@
 package com.example.publisher.service;
 
 
+import com.example.publisher.model.UsuarioModel;
+import com.example.publisher.repository.UsuarioRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class UsuarioService {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+    ModelMapper mapper;
+
+    public UsuarioService(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
+    public Mono<UsuarioModel> saveUsuario(UsuarioModel clienteModel) {
+        return this.usuarioRepository.save(clienteModel);
+    }
+    public Flux<UsuarioModel> listarTodos() {
+        return this.usuarioRepository.findAll();
+    }
 }
